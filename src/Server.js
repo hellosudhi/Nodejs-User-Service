81c0550d04f3c59d76c44f723f93710b3885f2f3
sudhiDevTeam <sudhi.astra@gmail.com>
2020-09-10 20:39:26 +0530

    A tests

--- src/Server.js
+++ src/Server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 9000
 
 require('./Config/DBConnection')()
 app.get('/vapaus', (request, response) => {
-  response.send('this is working')
+  response.send('this is working user service ')
 })
 app.get('/user', async (request, response) => {
   await user
